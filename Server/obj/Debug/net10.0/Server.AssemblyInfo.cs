89c718bbf28b20593f87faa7b776f4f634f99537
Rioni2324 <rb70043@ubt-uni.net>
2026-03-30 23:51:49 +0200

    All microservice structure and files -3

--- Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef02061e02e5ec52ec7ba5e82931fa316cc0f6d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+471c238f31d75b71b52b5674834c1a03a22b4b72")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
